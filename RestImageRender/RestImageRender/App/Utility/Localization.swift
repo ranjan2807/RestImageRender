a536b837465a92ad24610ea4a94473cabf11e053
Ranjan Patra <ranjan.patra@newpage.io>
2020-06-24 23:01:29 +0530

    Add Localization

---
+++ RestImageRender/RestImageRender/App/Utility/Localization.swift
@@ -0,0 +1,14 @@
+//
+//  Localization.swift
+//  OverdraftLimit
+//
+//  Copyright © 2020 Ranjan-iOS. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
