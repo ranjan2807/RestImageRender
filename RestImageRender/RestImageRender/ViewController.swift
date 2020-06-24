ec4eb38ee019c17a4ffcef350db35ef0e4ff0584
Ranjan Patra <ranjan.patra@newpage.io>
2020-06-24 22:52:16 +0530

    Add initial xcode project

---
+++ RestImageRender/RestImageRender/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestImageRender
+//
+//  Created by Newpage-iOS on 24/06/20.
+//  Copyright © 2020 Ranjan-iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
