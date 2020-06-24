485a3da1083fada3bf98a0abf7736f7b395f84fa
Ranjan Patra <ranjan.patra@newpage.io>
2020-06-25 00:24:41 +0530

    add constant file

---
+++ RestImageRender/RestImageRender/App/Utility/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  OverdraftLimit
+//
+//  Copyright © 2020 Ranjan-iOS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct ODColors {
+    static let primary: UIColor = #colorLiteral(red: 0.4980392157, green: 0.7960784314, blue: 0.8, alpha: 1)
+    static let secondary: UIColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    static let tertiary: UIColor = #colorLiteral(red: 0.9137254902, green: 0.9176470588, blue: 0.9215686275, alpha: 1)
+    static let danger: UIColor = #colorLiteral(red: 0.6274520159, green: 0.3507198095, blue: 0.3933730125, alpha: 1)
+}
+
