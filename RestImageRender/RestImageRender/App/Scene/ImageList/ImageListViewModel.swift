8aff260ebea06588847ab2795ba3f7f47d386bfb
Ranjan Patra <ranjan.patra@newpage.io>
2020-06-26 00:38:45 +0530

    Add rest client to test rest api

--- RestImageRender/RestImageRender/App/Scene/ImageList/ImageListViewModel.swift
+++ RestImageRender/RestImageRender/App/Scene/ImageList/ImageListViewModel.swift
@@ -11,18 +11,28 @@ import RxCocoa
 
 protocol ImageListViewModelProtocol {
     var titleObservable: Observable<String> { get }
+
     func initialize()
 }
 
 final class ImageListViewModel: ImageListViewModelProtocol {
 
     private let subject = BehaviorRelay<String>(value: "")
+    private let restClient: RestClientProtocol?
 
     var titleObservable: Observable<String> {
         return subject.asObservable()
     }
 
+    init(_ restClient: RestClientProtocol) {
+        self.restClient = restClient
+    }
+
     func initialize() {
-        subject.accept("Test")
+
+        guard let client = self.restClient else { return }
+        client.loadData { [unowned self] (data) in
+            self.subject.accept(data.title ?? "")
+        }
     }
 }
---
+++ RestImageRender/RestImageRender/App/Service/RestClient.swift
@@ -0,0 +1,40 @@
+//
+//  RestClient.swift
+//  RestImageRender
+//
+//  Created by Newpage-iOS on 25/06/20.
+//  Copyright © 2020 Ranjan-iOS. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+protocol RestClientProtocol {
+    func loadData(completion: @escaping (AppData) -> Void)
+}
+
+struct RestClient: RestClientProtocol {
+
+    func loadData( completion: @escaping (AppData) -> Void) {
+
+        AF.request(
+            "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+        ).validate()
+            .responseJSON { response in
+                print(response.data!)
+
+                let stringValue = String(decoding: response.data!, as: UTF8.self)
+                print(stringValue)
+
+                let dataTemp = Data(stringValue.utf8)
+
+                do {
+
+                    let appData = try JSONDecoder().decode(AppData.self, from: dataTemp)
+                    completion(appData)
+                } catch {
+                    print(error)
+                }
+        }
+    }
+}
