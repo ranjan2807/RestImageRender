47a281e657c7463bbcd34b6b26288acf2b59bc47
Ranjan Patra <ranjan.patra@newpage.io>
2020-06-28 02:20:43 +0530

    Remove unused pods RxDataSource and Kingfisher

--- RestImageRender/RestImageRender/App/Scene/ImageList/ImageListViewController.swift
+++ RestImageRender/RestImageRender/App/Scene/ImageList/ImageListViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 import RxSwift
-import Kingfisher
 
 final class ImageListViewController: UIViewController {
 
--- RestImageRender/RestImageRender/App/Scene/ImageList/ImageViewData.swift
+++ RestImageRender/RestImageRender/App/Scene/ImageList/ImageViewData.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 import RxSwift
-import Kingfisher
 
 struct ImageViewData {
 
