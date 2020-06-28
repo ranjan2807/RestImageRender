fefbdd1ffe9a87beb58c3a6a467c2fc9aee6c412
Ranjan Patra <ranjan.patra@newpage.io>
2020-06-28 13:25:29 +0530

    Code commenting for service classes

--- RestImageRender/RestImageRender/App/Service/CacheClient.swift
+++ RestImageRender/RestImageRender/App/Service/CacheClient.swift
@@ -8,18 +8,29 @@
 import Foundation
 import RxSwift
 
+
+/// Caching service
 struct CacheClient: ImageProcessStrategyProtocol {
 
+	/// fetch image from image folder of document directory
+	/// - Parameter url: remote url for referncing the local image name
 	func fetchImage(url: String) -> Observable<Data> {
 		return Observable.create { observer in
 
 			DispatchQueue.global().async {
 
+				/// retrieve cached file from document directory
 				if let data = FileOperations.fileDataFor(remoteUrl: url) {
+					// forward the data using observation and then finish up the observable
 					observer.onNext(data)
 					observer.onCompleted()
 				} else {
-					observer.onError(RIRError.factory.customError(domain: "Error retreiving data from local file"))
+					// forward the custom error.
+					observer.onError(RIRError.factory
+						.customError(
+							domain: "Error retreiving data from local file".localized
+						)
+					)
 				}
 			}
 			return Disposables.create()
--- RestImageRender/RestImageRender/App/Service/ImageDownloader.swift
+++ RestImageRender/RestImageRender/App/Service/ImageDownloader.swift
@@ -9,27 +9,73 @@ import Foundation
 import RxSwift
 import Swinject
 
+/// Strategy Protocol responsble for image fetching from url
 protocol ImageProcessStrategyProtocol {
+
+	/// method to fetch image data
+	/// - Parameter url: remote url of image data
 	func  fetchImage (url: String) -> Observable<Data>
 }
 
-struct ImageLoader {
+/// Protocol for Image fetcher which uses a specific ImageProcessStrategyProtocol client
+/// object to return back observable of image retreived either from remote or local.
+/// Client objects contains a specific logic which aims at retreiving image data
+/// This client object is dynamically injected using constructor base injection
+protocol ImageFetcherProtocol {
 
-	enum ClientType: String {
-		case rest = "rest-type"
-		case cache = "cache-type"
+	/// Designated contruction for image fetcher class which fetches
+	/// image from a given url using a specific logic defined by a dynamic client
+	/// - Parameters:
+	///   - url: remote url of image
+	///   - client: client containing a specific logic to retrieve data
+	init (url: String, client: ImageProcessStrategyProtocol)
+
+	/// Starts loading of image from the given remote url using a specific client logic
+	func loadImage() -> Observable<Data>
+}
+
+/// Class which implements ImageFetcherProtocol and aims at retreiving
+/// image from a given url and a dynamic client logic (Strategy design pattern)
+struct ImageFetcherStrategy: ImageFetcherProtocol {
+	// hold remote url
+	private var url: String
+	// hold a specific client logic
+	private var client: ImageProcessStrategyProtocol
+
+	init (url: String, client: ImageProcessStrategyProtocol) {
+		self.url = url
+		self.client = client
 	}
 
+	func loadImage() -> Observable<Data> {
+		return client.fetchImage(url: url)
+	}
+}
+
+
+/// Main class responsible for proving image
+struct ImageLoader {
+
+	/// Enum for image fetcher client type
+	enum ClientType: String {
+		case rest = "rest-type" // rest api client type
+		case cache = "cache-type" // local cache client type
+	}
+
+	/// shared instance for image downloader class
 	static let shared = ImageLoader()
 
+	/// Swinject container for handling strategy client dependency injection
 	private var container: Container = {
 		var contTemp = Container()
 
+		// register for rest client type dependency
 		contTemp.register(ImageProcessStrategyProtocol.self,
 						  name: ClientType.rest.rawValue) { _ in
 			return RestClient()
 		}
 
+		// register for local cache client type dependency
 		contTemp.register(ImageProcessStrategyProtocol.self,
 						  name: ClientType.cache.rawValue) { _ in
 			return CacheClient()
@@ -38,10 +84,15 @@ struct ImageLoader {
 		return contTemp
 	} ()
 
+	/// Make constructor private
 	private init () {}
 
+	/// Starts retreiving of image data from a specific url
+	/// - Parameter url: url of image
 	func retreiveImage(_ url: String) -> Observable<Data> {
-
+		/// Checks if image is available in local
+		/// if yes, then go for local cache
+		// if no, go for remote downloading of image
 		var clientType: ClientType
 
 		if FileOperations.checkFileIsAvailable(remoteUrl: url) {
@@ -53,26 +104,8 @@ struct ImageLoader {
 		let client = container.resolve(ImageProcessStrategyProtocol.self,
 									   name: clientType.rawValue)!
 
-		let fetcher = ImageFetchStrategy(url: url, client: client)
+		/// Start image fetcher using a specific client type
+		let fetcher = ImageFetcherStrategy(url: url, client: client)
 		return fetcher.loadImage()
-	}
-}
-
-protocol ImageFetchProtocol {
-	init (url: String, client: ImageProcessStrategyProtocol)
-	func loadImage() -> Observable<Data>
-}
-
-struct ImageFetchStrategy: ImageFetchProtocol {
-	private var url: String
-	private var client: ImageProcessStrategyProtocol
-
-	init (url: String, client: ImageProcessStrategyProtocol) {
-		self.url = url
-		self.client = client
-	}
-
-	func loadImage() -> Observable<Data> {
-		return client.fetchImage(url: url)
 	}
 }
